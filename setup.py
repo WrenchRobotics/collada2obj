66ed2bfededbcf223642d5bb86abaf3716dc4e75
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2024-08-25 23:40:03 -0400

    Kr/expand readme1 (#2)
    * Updated readme.md with more detail
    
    * Small updates to readme.md

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import codecs
 import os
 
 if __name__ == "__main__":
-    # here = os.path.abspath(os.path.dirname(__file__))
-    #
-    # with codecs.open(
-    #     os.path.join(here, "README.md"), encoding="utf-8"
-    # ) as f:
-    #     long_description = "\n" + f.read()
+    here = os.path.abspath(os.path.dirname(__file__))
+
+    with codecs.open(
+        os.path.join(here, "README.md"), encoding="utf-8"
+    ) as f:
+        long_description = "\n" + f.read()
 
     setup(
         name="collada2obj",
@@ -16,12 +16,12 @@ if __name__ == "__main__":
         author="Kwesi Rutledge",
         author_email="thesolitaryecrivain@gmail.com",
         url="https://github.com/kwesiRutledge/collada2obj",
-        # description="A set of convenient logging and testing tools for the Drake robotics toolbox.",
-        # long_description_content_type="text/markdown",
-        # long_description=long_description,
+        description="A library that can be used to easily convert a collada file (.dae) to a wavefront file (.obj).",
+        long_description_content_type="text/markdown",
+        long_description=long_description,
         packages=find_packages(),
         install_requires=['ipdb', 'typer', 'numpy'],
-        keywords=["robotics", "testing", "logging", "dae"],
+        keywords=["robotics", "3D Modelling", "dae", "obj"],
         # classifiers=[
         #     "Programming Language :: Python :: 3",
         # ]
