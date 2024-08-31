d54f2a8a64311929e1d68d847531f24094d0c856
kwesirutledge <thesolitaryecrivain@gmail.com>
2024-08-31 09:55:38 -0400

    Basic Support for Polylist (Should we move to pycollada)

---
+++ examples/convert_base_containing_polylist/readme.md
@@ -0,0 +1,3 @@
+# Convert Base to an `.obj` file when collada file contains polylist
+
+This example attempts to convert a collada file to an `.obj` file using the `collada2obj` library. The collada file contains a polylist, which is a more complex geometry type than a simple triangle list. This example is used to test the `collada2obj` library's ability to handle more complex geometry types.
---
+++ test/ColladaFileConverter_test.py
@@ -0,0 +1,38 @@
+"""
+ColladaFileConverter_test.py
+Description
+-----------
+This file contains tests for the ColladaFileConverter class.
+"""
+
+from importlib import resources as impresources
+import unittest
+from os import makedirs
+
+# Local imports
+import collada2obj
+from collada2obj import ColladaFileConverter
+
+class TestColladaFileConverter(unittest.TestCase):
+    def test_determine_sources_technique_common1(self):
+        """
+        Description
+        -----------
+        This test checks the determine_sources_technique_common method.
+        :return:
+        """
+        # Setup
+        dae_filename = str(
+            impresources.files(collada2obj) / "../examples/convert_base_containing_polylist/base.dae"
+        )
+        converter = ColladaFileConverter(str(dae_filename), obj_filename="./out.obj")
+
+        # Test
+        found_company_name = False
+        for line in converter.get_preamble_lines():
+            if "Wrench Robotics" in line:
+                found_company_name = True
+        self.assertTrue(found_company_name)
+
+if __name__ == '__main__':
+    unittest.main()
