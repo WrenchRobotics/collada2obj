03dbeb7c166f85f9f3c9b2afffd42749f2ee8aeb
kwesirutledge <thesolitaryecrivain@gmail.com>
2024-08-31 10:19:56 -0400

    Improved comments

--- test/MeshConverter_test.py
+++ test/MeshConverter_test.py
@@ -55,7 +55,7 @@ class TestMeshConverter(unittest.TestCase):
         Description
         -----------
         This test checks the find_triangles method when a mesh does not have the "triangles" tag
-        inside the mesh eleemnt.
+        inside the mesh element, but it does have the "polylist" tag.
         :return:
         """
         # Setup
@@ -87,7 +87,37 @@ class TestMeshConverter(unittest.TestCase):
             len(p_text_str) // (3*2),
             int(polylist_elt.attrib['count']))
 
+    def test_find_triangles2(self):
+        """
+        Description
+        -----------
+        This test checks the find_triangles method when a mesh does not have the "triangles" or "polylist" tag
+        inside the mesh eleemnt.
+        :return:
+        """
+        # Setup
+        dae_filename = str(
+            impresources.files(collada2obj) / "../examples/convert_base_containing_polylist/base.dae"
+        )
 
+        # Extract the mesh
+        tree = ET.ElementTree(file=dae_filename)
+
+        # FIX xmlns problem
+        # http://stackoverflow.com/questions/13412496/python-elementtree-module-how-to-ignore-the-namespace-of-xml-files-to-locate-ma
+        for el in tree.iter():
+            if '}' in el.tag:
+                el.tag = el.tag.split('}', 1)[1]  # strip all namespaces
+
+        meshes = tree.findall('library_geometries/geometry/mesh')
+
+        # Create converter and make sure that it doesn't produce an error when the mesh doesn't have triangles
+        converter = MeshConverter(meshes[0])
+        try:
+            triangles = converter.find_triangles()
+            self.assertTrue(False)
+        except Exception as e:
+            self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
